7070d76f933bae6ecbe479089ce2ad136acbcad0
Alistair Dutton <alistair.dutton@nokia.com>
2013-11-16 21:55:21 +0000

    Refactoring of tests.

---
+++ test/lib/vanilli-helper.js
@@ -0,0 +1,53 @@
+var vanilli = require('../../lib/vanilli.js'),
+    restify = require('restify'),
+    portfinder = require('portfinder'),
+    servers, vanilliPort, apiPort;
+
+portfinder.basePort = 14000;
+
+exports.start = function(log, done) {
+    function findFreePorts(numberOfPorts, callback) {
+        function portFound(err, port) {
+            if (err) {
+                throw new Error(err);
+            } else {
+                ports.push(port);
+                if (ports.length === numberOfPorts) {
+                    callback(ports);
+                }
+            }
+        }
+
+        var ports = [], i;
+
+        for (i = 1; i <= numberOfPorts; i++) {
+            portfinder.getPort(portFound);
+        }
+    }
+
+    findFreePorts(2, function (ports) {
+        apiPort = ports[0];
+        vanilliPort = ports[1];
+
+        servers = vanilli.startServer({ apiPort: apiPort, vanilliPort: vanilliPort, log: log });
+
+        done(servers);
+    });
+};
+
+exports.stop = function() {
+    servers.closeAll();
+};
+
+exports.createApiClient = function () {
+    return restify.createJsonClient({
+        url: 'http://localhost:' + apiPort
+    });
+};
+
+exports.createVanilliClient = function () {
+    return restify.createJsonClient({
+        url: 'http://localhost:' + vanilliPort
+    });
+};
+
--- test/vanilli-test.js
+++ test/vanilli-test.js
@@ -1,53 +1,38 @@
 var expect = require('chai').expect,
     vanilli = require('../lib/vanilli.js'),
-    restify = require('restify'),
-    portScanner = require('./lib/port-scanner.js'),
+    vanilliHelper = require('./lib/vanilli-helper.js'),
     log = require('bunyan').createLogger({
         name: "vanilli-test",
         level: "error"
     });
 
 describe('Vanilli', function () {
-    var servers, vanilliPort, apiPort, vanilliClient, apiClient;
-
-    function startServers(done) {
-        portScanner.findFreePorts(2, function (ports) {
-            apiPort = ports[0];
-            vanilliPort = ports[1];
-
-            servers = vanilli.startServer({ apiPort: apiPort, vanilliPort: vanilliPort, log: log });
-
-            apiClient = restify.createJsonClient({
-                url: 'http://localhost:' + apiPort
-            });
-
-            vanilliClient = restify.createJsonClient({
-                url: 'http://localhost:' + vanilliPort
-            });
-
-            done();
-        });
-    }
-
-    function stopServers() {
-        servers.closeAll();
-    }
-
     it('throws an error if no api port specified', function () {
-        expect(function () { vanilli.startServer({ apiPort: 1234 }); }).to.throw(/vanilliPort not specified/);
+        expect(function () {
+            vanilli.startServer({ apiPort: 1234 });
+        })
+            .to.throw(/vanilliPort not specified/);
     });
 
     it('throws an error if no vanilli port specified', function () {
-        expect(function () { vanilli.startServer({ vanilliPort: 1234 }); }).to.throw(/apiPort not specified/);
+        expect(function () {
+            vanilli.startServer({ vanilliPort: 1234 });
+        })
+            .to.throw(/apiPort not specified/);
     });
 
     describe('API', function () {
+        var apiClient;
+
         before(function (done) {
-            startServers(done);
+            vanilliHelper.start(log, function () {
+                apiClient = vanilliHelper.createApiClient();
+                done();
+            });
         });
 
         after(function () {
-            stopServers();
+            vanilliHelper.stop();
         });
 
         it('should be pingable', function (done) {
@@ -59,12 +44,17 @@ describe('Vanilli', function () {
     });
 
     describe('Server', function () {
+        var vanilliClient;
+
         before(function (done) {
-            startServers(done);
+            vanilliHelper.start(log, function () {
+                vanilliClient = vanilliHelper.createVanilliClient();
+                done();
+            });
         });
 
         after(function () {
-            stopServers();
+            vanilliHelper.stop();
         });
 
         it('should be pingable', function (done) {
